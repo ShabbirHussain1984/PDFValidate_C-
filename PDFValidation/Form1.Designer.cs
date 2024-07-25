9199a813c99b7cde711d4e7bf66fc5b7e87ec180
ShabbirHussain1984 <164902870+ShabbirHussain1984@users.noreply.github.com>
2024-07-25 17:51:04 +0500

    first commit
    first commit

---
+++ PDFValidation/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PDFValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSignedData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.txtFileName.Location = new System.Drawing.Point(17, 133);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(753, 34);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.btnValidate.Location = new System.Drawing.Point(283, 173);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(217, 72);
+            this.btnValidate.TabIndex = 1;
+            this.btnValidate.Text = "Select File";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.lblFileName.Location = new System.Drawing.Point(12, 95);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(99, 26);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "File Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.label1.Location = new System.Drawing.Point(322, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PDF Validation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14F);
+            this.label2.Location = new System.Drawing.Point(12, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Data";
+            // 
+            // txtSignedData
+            // 
+            this.txtSignedData.Font = new System.Drawing.Font("Comic Sans MS", 12F);
+            this.txtSignedData.Location = new System.Drawing.Point(17, 282);
+            this.txtSignedData.Multiline = true;
+            this.txtSignedData.Name = "txtSignedData";
+            this.txtSignedData.Size = new System.Drawing.Size(753, 238);
+            this.txtSignedData.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 532);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSignedData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSignedData;
+    }
+}
+
---
+++ PDFValidation/Form1.cs
@@ -0,0 +1,82 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ValidateSignature(string pdfPath)
+        {
+            try
+            {
+                PdfDocument pdfDoc = new PdfDocument(new PdfReader(pdfPath));
+                SignatureUtil signUtil = new SignatureUtil(pdfDoc);
+                IList<string> names = signUtil.GetSignatureNames();
+
+                txtSignedData.Text = "N/A";
+
+                if (names.Count == 0)
+                {
+                    txtSignedData.Text = "The selected PDF file has not been signed.";
+                }
+                else
+                {
+                    foreach (string name in names)
+                    {
+                        PdfPKCS7 pkcs7 = signUtil.ReadSignatureData(name);
+
+                        bool isSignatureVerified = pkcs7.VerifySignatureIntegrityAndAuthenticity();
+
+                        txtSignedData.Text = "";
+                        txtSignedData.Text = txtSignedData.Text + ($"Signature {name} is {(isSignatureVerified ? "valid" : "invalid")}") + System.Environment.NewLine;
+
+                        // Print signer info
+                        txtSignedData.Text = txtSignedData.Text + ($"Subject: {pkcs7.GetSigningCertificate().GetSubjectDN()}") + System.Environment.NewLine;
+                        txtSignedData.Text = txtSignedData.Text + ($"Issuer: {pkcs7.GetSigningCertificate().GetSubjectDN()}") + System.Environment.NewLine;
+                        txtSignedData.Text = txtSignedData.Text + ($"Signed on: {pkcs7.GetSignDate()}") + System.Environment.NewLine;
+
+                        // You can also print more detailed certificate information here if needed
+
+                        return;
+                    }
+
+                    txtSignedData.Text = "The selected PDF file is signed.";
+                }
+            }
+            catch (Exception ex)
+            {
+                txtSignedData.Text = ex.Message;
+            }
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            DialogResult objDialogResult = openFileDialog1.ShowDialog();
+
+            openFileDialog1.InitialDirectory = "C:\\Users\\hussain\\Desktop\\SignSample";
+            openFileDialog1.Filter = "PDF files (*.pdf)|*.pdf";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (objDialogResult == DialogResult.OK)
+            {
+                txtFileName.Text = openFileDialog1.FileName;
+                ValidateSignature(txtFileName.Text);
+            }
+        }
+    }
+}
